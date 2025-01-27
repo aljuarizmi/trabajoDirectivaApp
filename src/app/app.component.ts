a2ead62280c68465333b5e47308ed3f9ae0c4b71
aljuarizmi <aljuarizmi@gmail.com>
2025-01-27 12:48:55 -0500

    Nueva app angular con directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registro de Usuarios';
+  mensaje="";
+  registrado=false;
+  nombre:string="";
+  apellido:string="";
+  entradas:object[];
+  constructor(){
+    this.entradas=[
+      {cabecera:"Phyton cada día más presente"},
+      {cabecera:"Java presente desde hace más de veinte años"},
+      {cabecera:"Javascript cada vez más funcional"},
+      {cabecera:"Kotlin, potencia para tus apps"},
+      {cabecera:"¿Dónde quedó pascal?"},
+    ]
+  };
+  registrarUsuario(){
+    this.registrado=true;
+    this.mensaje="Usuario registrado con éxito";
+  }
 }
